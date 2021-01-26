03a05774bdfce8e6b32a3fd9d9e38a0d284ce445
yegahgulnihal <yegahdegirmenci@gmail.com>
2021-01-26 16:34:40 +0300

    Add project files.

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> MyList = new Dictionary<int, string>();
+            MyList.Add(1, "Yegah");
+            MyList.Add(2,"Elif");
+            MyList.Add(3, "Mirza");
+
+            foreach ( KeyValuePair<int,string> person in MyList)
+            {
+               Console.WriteLine(" {0}. person is {1}",person.Key,person.Value);
+            }
+
+        }
+    }
+}
